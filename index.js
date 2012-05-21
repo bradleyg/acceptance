0ba0937f25dc9bcde11145589432260e9676aeef
bradley griffiths <bradley.griffiths@gmail.com>
2012-05-21 17:52:49 +0100

    don't break api

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-module.exports = require('./lib/acceptance');
+module.exports.accept = function(params, schema) {
+  var errors = [];
+  var accepted = {};
+
+  var addError = function(key, message) {
+    errors.push({
+      field: key,
+      message: message
+    });
+  }
+
+  Object.keys(schema).forEach(function(key){
+    var option = schema[key]
+    var value = params[key]
+    var validate = (value && option.validation)
+    
+    if(validate && ! value.match(option.validation.regex)){
+      addError(key, option.validation.message)
+    }
+    else if(option.required && ! value) addError(key, 'Missing field')
+    else if(value) accepted[key] = value
+  });
+
+  return {
+    errors: errors.length ? errors : null,
+    accepted: accepted
+  };
+}
---
+++ test/acceptance.js
@@ -0,0 +1,54 @@
+var should = require('should');
+var accept = require('../index').accept;
+
+var schema = {
+  name: {
+    required: true,
+    validation: { regex: /^[a-zA-Z0-9]+$/, message: 'Not alpha numeric' }
+  },
+  phone: {
+    required: false,
+    validation: { regex: /^-?[0-9]+$/, message: 'Not a valid integer' }
+  },
+  location: {
+    required: true
+  }
+}
+
+describe('acceptance', function(){
+  
+  describe('accept()', function(){
+    
+    it('should return an error if a field failed validation', function(){
+      var params = { phone: '012345hello' };
+      var a = accept(params, schema);
+      should.exist(a);
+      should.exist(a.errors);
+      should.exist(a.accepted);
+      a.errors[1].should.have.property('field', 'phone');
+      a.errors[1].should.have.property('message', 'Not a valid integer');
+    });
+    
+    it('should return an error if a required field is missing', function(){
+      var params = { phone: '012345hello' };
+      var a = accept(params, schema);
+      should.exist(a);
+      should.exist(a.errors);
+      should.exist(a.accepted);
+      a.errors[2].should.have.property('field', 'location');
+      a.errors[2].should.have.property('message', 'Missing field');
+    });
+    
+    it('should not return an error an error if valid fields are sent', function(){
+      var params = { name: 'Bradley', location: 'london' };
+      var a = accept(params, schema);
+      should.exist(a);
+      should.exist(a.accepted);
+      should.not.exist(a.errors);
+      a.accepted.should.have.property('name', 'Bradley');
+      a.accepted.should.have.property('location', 'london');
+    });
+      
+  });
+  
+});
